4516b7e714c1b8df63f6fa741dd67126e85d8ef3
cks <cksun2598@gmail.com>
2022-09-02 21:50:52 +0900

    d

---
+++ src/com/kh/practice/list/library/model/vo/Book.java
@@ -0,0 +1,14 @@
+package com.kh.practice.list.library.model.vo;
+
+public class Book {
+	
+	private String title;
+	private String author;
+	private String category;
+	private int price;
+	
+	public Book() {}
+	
+	
+
+}
