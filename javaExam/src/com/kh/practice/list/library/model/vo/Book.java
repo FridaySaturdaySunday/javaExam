73c9d021ba8cfd38fa1d73ecf67defc2d6ca5fbd
cks <cksun2598@gmail.com>
2022-09-05 01:48:56 +0900

    d

--- javaExam/src/com/kh/practice/list/library/model/vo/Book.java
+++ javaExam/src/com/kh/practice/list/library/model/vo/Book.java
@@ -7,4 +7,97 @@ public class Book {
 	private String category;
 	private int price;
 	
+	public Book() {}
+	
+	public Book(String title, String author, String category, int price) {
+		this.title = title;
+		this.author = author;
+		this.category = category;
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "(" + title + "/" + author + "/" + category + "/" + price + ")";
+	}
+
+
+	@Override
+	public int hashCode() { // 해시코드를 리턴하는 메소드
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((author == null) ? 0 : author.hashCode());
+		result = prime * result + ((category == null) ? 0 : category.hashCode());
+		result = prime * result + price;
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) { //객체가 같은지 여부를 리턴하는 메소드
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (author == null) {
+			if (other.author != null)
+				return false;
+		} else if (!author.equals(other.author))
+			return false;
+		if (category == null) {
+			if (other.category != null)
+				return false;
+		} else if (!category.equals(other.category))
+			return false;
+		if (price != other.price)
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+	
+	public int compareTo(Object o) { // 정렬 기준을 가지고 있는 메소드
+		int result = 0;
+		
+		return result;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	public String getCategory() {
+		return category;
+	}
+	
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
 }
